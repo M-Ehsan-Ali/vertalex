033e481edd5d4a53c2e0f5ce72ecbc0316d59bf6
Muhammad-Ehsan-Ali <Muhammad.Ehsan@46labs.com>
2026-02-20 14:32:14 +0500

    logo changed for client on 20 Feb 2026

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-"use client";
-import Link from "next/link";
+'use client'
+import Link from 'next/link'
 
 export default function Header() {
   return (
@@ -8,21 +8,21 @@ export default function Header() {
         {/* Fixed Header */}
         <div className="bg-[#ffffff] pl-4 lg:pl-[62px] pr-5 lg:pr-[62px] flex items-center justify-between 2xl:static fixed top-0 left-0 w-[100vw] sm:w-full z-40 h-[100px] border-b border-[#D9DEDD]">
           {/* Logo Section */}
-          <Link href={"/"}>
+          <Link href={'/'}>
             <div className="hidden xl:flex flex-col justify-center items-center">
               <img
-                src="/logo-light.svg"
+                src="/footerLogo.svg"
                 alt="vertalex"
                 loading="lazy"
-                className="mt-[8px] w-[270px] h-[50px]"
+                className="mt-[8px] h-[80px]"
               />
             </div>
             <div className="flex flex-col justify-center items-center xl:hidden">
               <img
-                src="/logo-light.svg"
+                src="/footerLogo.svg"
                 loading="lazy"
                 alt="vertalex"
-                className=" mt-[8px] w-[120px] h-[20px]"
+                className=" mt-[8px] h-[60px]"
               />
             </div>
           </Link>
@@ -31,7 +31,7 @@ export default function Header() {
           <div className="flex gap-[12px] sm:gap-x-5 justify-center">
             <div>
               <Link
-                href={"#why-us"}
+                href={'#why-us'}
                 className="font-rubik text-[10px] sm:text-[16px] font-medium leading-6 tracking-tight text-center decoration-skip-ink"
               >
                 Why us
@@ -39,7 +39,7 @@ export default function Header() {
             </div>
             <div>
               <Link
-                href={"#our-projects"}
+                href={'#our-projects'}
                 className="font-rubik text-[10px] sm:text-[16px] font-medium leading-6 tracking-tight text-center decoration-skip-ink"
               >
                 Our Projects
@@ -47,7 +47,7 @@ export default function Header() {
             </div>
             <div>
               <Link
-                href={"#in-news"}
+                href={'#in-news'}
                 className="font-rubik text-[10px] sm:text-[16px] font-medium leading-6 tracking-tight text-center decoration-skip-ink"
               >
                 News
@@ -57,10 +57,10 @@ export default function Header() {
           {/* <button className="hidden">Get in touch</button> */}
 
           <Link
-            href={"#contact-us"}
+            href={'#contact-us'}
             className="flex items-center gap-[0.8px] sm:gap-[8px] border border-[#64ce5b] px-[0.8px] py-[0.8px] sm:px-5 sm:py-3 rounded-full font-rubik font-medium text-[10px] sm:text-[16px] leading-6 tracking-tight text-[#1d1f1e] decoration-skip-ink"
           >
-            {"Get in touch "}
+            {'Get in touch '}
             <img
               src={`/tailIcon.svg`}
               alt="Tail"
@@ -70,5 +70,5 @@ export default function Header() {
         </div>
       </div>
     </div>
-  );
+  )
 }
